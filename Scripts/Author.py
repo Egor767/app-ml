308e336ee2884842aeeb4c7a02415ef75b535eae
Egor Musiyak <musiyak26@mail.ru>
2024-01-10 19:39:08 +0600

    Scripts

---
+++ Scripts/Author.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Разработка Web-приложения для инференса моделей ML и анализа данных")
+
+st.header("Автор работы:")
+
+st.subheader("Мусияк Егор Алексеевич")
+st.divider()
+st.subheader("Студент группы МО-221")
+st.divider()
+st.subheader("Фото")
+img = Image.open("image.jpg")
+st.image(img, width=250)
