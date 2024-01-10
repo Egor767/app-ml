1ed1e094833625558820d6ad0216d4c539622d9a
Egor Musiyak <musiyak26@mail.ru>
2024-01-10 20:40:38 +0600

    1

--- Scripts/dada/Visual.py
+++ Scripts/dada/Visual.py
@@ -3,12 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 
+
 def HeatMap():
     copy = data.drop(["map"], axis=1)
     st.header("Тепловая карта")
     plt.figure(figsize=(16,10))
     sns.heatmap(copy.corr(), annot=True, cmap='coolwarm')
     st.pyplot(plt)
+
 
 def Hist():
     st.header("Гистограмма распределения")
@@ -18,6 +20,7 @@ def Hist():
     sns.histplot(data[select])
     st.pyplot(plt)
 
+
 def BoxPlot():
     st.header("Диаграмма Ящик с усами")
     features = data.columns.to_list()
@@ -25,6 +28,7 @@ def BoxPlot():
     plt.figure(figsize=(12,7))
     sns.boxplot(x = data[select])
     st.pyplot(plt)
+
 
 def Scatter():
     st.header("Диаграмма рассеяния")
@@ -34,6 +38,7 @@ def Scatter():
     plt.figure(figsize=(12,7))
     plt.scatter(data[select1][:500], data[select2][:500], )
     st.pyplot(plt)
+
 
 data = pd.read_csv("Data\csgo_taskv2.csv")
 data = data.drop(["Unnamed: 0"], axis=1)
